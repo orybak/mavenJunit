20516641ccc5cbc4634a0582b8f4e2b6bc1dabcf
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-10-18 10:52:13 +0200

    Update MavenDemoApplicationTests.java

--- src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationTests.java
+++ src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationTests.java
@@ -3,10 +3,10 @@ package com.jetbrains.mavendemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "foo=bar")
 class MavenDemoApplicationTests {
-String foo;
-	foo = "foo";
+  @Value("${foo}")
+  String foo;
 	@Test
 	void contextLoads() {
 		assertThat(foo).isEqualTo("bar");
