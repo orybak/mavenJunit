534324abca7cc20c7c578601e2914ec131d06902
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-10-18 10:56:00 +0200

    Update MavenDemoApplicationTests.java

--- src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationTests.java
+++ src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.jetbrains.mavendemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MavenDemoApplicationTests {
