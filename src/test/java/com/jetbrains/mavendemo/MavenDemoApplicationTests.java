2f1765de992e7ab1509e034180811d043b2960a3
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-10-18 10:56:52 +0200

    Update MavenDemoApplicationTests.java

--- src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationTests.java
+++ src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationTests.java
@@ -1,8 +1,15 @@
 package com.jetbrains.mavendemo;
 
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MavenDemoApplicationTests {
