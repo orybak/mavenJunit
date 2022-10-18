43619f0e122932afe9aa67cbad03986b0ea68a11
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-10-18 11:01:31 +0200

    Create AppTests2.java

---
+++ src/test/java/com/jetbrains/mavendemo/AppTests2.java
@@ -0,0 +1,33 @@
+package com.jetbrains.mavendemo;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests2 {
+
+	@Test
+	void contextLoads21() {
+		assertThat("foo").isEqualTo("foo");
+
+	}
+		@Test
+	void contextLoads22() {
+		assertThat("foo").isEqualTo("foo");
+
+	}
+		@Test
+	void contextLoads23() {
+		assertThat("foo").isEqualTo("foo");
+
+	}
+
+}
