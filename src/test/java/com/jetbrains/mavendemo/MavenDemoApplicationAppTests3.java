21fc4bc0528a3e4f23775e8f166d0cadf84015af
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-10-18 11:04:50 +0200

    Update and rename AppTests3.java to MavenDemoApplicationAppTests3.java

--- src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationAppTests3.java
+++ src/test/java/com/jetbrains/mavendemo/MavenDemoApplicationAppTests3.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppTests3 {
+class MavenDemoApplicationAppTests3 {
 
 	@Test
 	void contextLoads11() {
